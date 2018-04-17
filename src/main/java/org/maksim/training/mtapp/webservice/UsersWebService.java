f4461439aaec6bfc19c4245b4037736c3faacafc
Maksim Patapenka <maksim.patapenka@gmail.com>
2018-04-17 21:26:47 +0300

    WS

---
+++ src/main/java/org/maksim/training/mtapp/webservice/UsersWebService.java
@@ -0,0 +1,27 @@
+package org.maksim.training.mtapp.webservice;
+
+import org.maksim.training.mtapp.entity.User;
+import org.maksim.training.mtapp.service.UserService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.ws.server.endpoint.annotation.Endpoint;
+import org.springframework.ws.server.endpoint.annotation.PayloadRoot;
+import org.springframework.ws.server.endpoint.annotation.RequestPayload;
+import org.springframework.ws.server.endpoint.annotation.ResponsePayload;
+
+@Endpoint
+public class UsersWebService {
+    private static final String NAMESPACE_URI = "http://mtapp.org/ws";
+
+    private final UserService userService;
+
+    @Autowired
+    public UsersWebService(UserService userService) {
+        this.userService = userService;
+    }
+
+    @PayloadRoot(namespace = NAMESPACE_URI, localPart = "user")
+    @ResponsePayload
+    public User getUser(@RequestPayload User user) {
+        return userService.getById(user.getId());
+    }
+}
